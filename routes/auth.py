c309ff02c98e87c9e002131d3a88a45a18eaea0a
Eugene Sharak <jekassharak@gmail.com>
2023-11-11 16:04:24 +0200

    fixlogin

--- routes/auth.py
+++ routes/auth.py
@@ -16,18 +16,16 @@ async def register(user: UserIn):
     user_is_existed = db.user.find_one({'email': user.email})
     if user_is_existed:
         return JSONResponse({'message': 'User is already in db'}, 400)
-    
+    db.user.insert_one({'user': user.model_dump()})
+    return JSONResponse({'message': 'User has been added to database!'})
 
-async def handle_register(user: UserIn):
-    user.password: str = bcrypt.hashpw(user.password.encode())
-    new_user: User = User.parse_obj(user)
 
 @auth.post('/auth/login')
 async def login(user_input: UserIn) -> JSONResponse:
-    login_user(user_input)
+    return login_user(user_input)
 
 def login_user(user_input: UserIn) -> JSONResponse:
-    user: Dict = db.user.find_one({'email': user_input.email})
+    user = db.user.find_one({'email': user_input.email})
     if user:
         if check_password(user_input.password, user['password']):
             unique_id = str(user['_id'])
@@ -35,6 +33,7 @@ def login_user(user_input: UserIn) -> JSONResponse:
             return JSONResponse({'accessToken': token, 'message': 'Success!'}, 200)
         return JSONResponse({'message': 'Incorrect password'}, 400)
     return JSONResponse({'message': 'User is not found'}, 400)
+
 
 async def create_token(unique_id: str) -> str:
     expiration_time = datetime.utcnow() + timedelta(days=2)
